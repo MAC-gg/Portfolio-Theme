79f55a6f832b57a98056fce0b7e239cfee39b422
MAC-gg <mc669756@gmail.com>
2021-03-18 21:13:28 -0400

    add files and connect them

---
+++ header.php
@@ -0,0 +1,6 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <?php wp_head(); ?>
+    </head>
+    <body style="background-image: url(<?php echo get_theme_file_uri('/img/main-background-wood-grain.jpg'); ?>);">
