5330cae0dbb6ed0d31a3deda8f73b531c4bdbe0d
MAC-gg <mc669756@gmail.com>
2021-03-20 13:30:40 -0400

    add basic WordPress settings and classes

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <DOCTYPE html>
-<html>
+<html <?php language_attributes(); // language setting ?>>
     <head>
+        <meta charset="<?php bloginfo('charset'); // based on language setting ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
-    <body style="background-image: url(<?php echo get_theme_file_uri('/img/main-background-wood-grain.jpg'); ?>);">
+    <body <?php body_class(); // WordPress added classes ?> style="background-image: url(<?php echo get_theme_file_uri('/img/main-background-wood-grain.jpg'); ?>);">
